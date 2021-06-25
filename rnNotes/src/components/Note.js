0519c60537a9ddfaa469511c76bfdb1bad330ffc
Колобова Ксения Евгеньевна <shrimpksko@gmail.com>
2021-06-25 14:15:40 +0700

    Добавлена иконка для перехода на экран добавления на главном экране в навигационной панели

--- rnNotes/src/components/Note.js
+++ rnNotes/src/components/Note.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+Icon.loadFont();
 
 import { APP_COLORS } from '../components/APP_COLORS';
 import { AppMainTitle } from './ui/AppMainTitle';
@@ -13,7 +15,10 @@ export const Note = ({ route }) => {
 
     return (
         <View style={styles.container}>
-            <AppMainTitle>{thisNote.map(note => note.noteName)}</AppMainTitle>
+            <View style={styles.titleContainer}>
+                <AppMainTitle>{thisNote.map(note => note.noteName)}</AppMainTitle>
+                <Icon name="create-outline" size={30} color={APP_COLORS.BROWN} />
+            </View>
             <View style={styles.description}>
                 <AppText style={{fontSize: 16, margin: LAYOUT_BLANKS.outerPadding}}>
                     {thisNote.map(note => note.noteText)}
@@ -28,6 +33,11 @@ const styles = StyleSheet.create({
         height: '100%',
         backgroundColor: APP_COLORS.LIGTH_YELLOW,
         paddingHorizontal: LAYOUT_BLANKS.innerPadding
+    },
+    titleContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
 	description: {
 		marginTop: LAYOUT_BLANKS.outerPadding, 
--- rnNotes/src/navigation/AppNavigation.js
+++ rnNotes/src/navigation/AppNavigation.js
@@ -2,25 +2,42 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+Icon.loadFont();
 
 import { MainScreen } from '../components/screens/MainScreen'
 import { SignInScreen } from '../components/screens/SignInScreen'
 import { CreateNoteScreen } from '../components/screens/CreateNoteScreen'
 import { NotesList } from '../components/NotesList';
 import { Note } from '../components/Note';
+import { APP_COLORS } from '../components/APP_COLORS';
+import { LAYOUT_BLANKS } from '../components/LAYOUT_BLANKS';
 
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
 
-export const AppNavigation = () => {
+export const AppNavigation = ({ navigation }) => {
     createHomeStack = () => (
         <Stack.Navigator>
             <Stack.Screen 
                 name="MainScreen" 
                 component={MainScreen} 
-                options={{
-                    title: "Мои заметки"
-                }}
+                options={({route, navigation}) => ({
+                    title: "Мои заметки",
+                    headerRight: () => (
+                        <Icon 
+                            name="add-circle-outline" 
+                            size={30} 
+                            color={APP_COLORS.BROWN} 
+                            style={{
+                                paddingRight: LAYOUT_BLANKS.outerPadding
+                            }}
+                            onPress={() => navigation.navigate('CreateNote')}
+                        />
+                     )
+                   }
+                )}
             />
             <Stack.Screen 
                 name="NotesList" 
@@ -34,6 +51,13 @@ export const AppNavigation = () => {
                 component={Note} 
                 options={{
                     title: "Заметка"
+                }}
+            />
+            <Stack.Screen 
+                name="CreateNote" 
+                component={CreateNoteScreen} 
+                options={{
+                    title: "Добавить заметку"
                 }}
             />
         </Stack.Navigator>
@@ -75,7 +99,10 @@ export const AppNavigation = () => {
                     name="Main" 
                     children={createHomeStack}
                     options={{
-                        title: "Мои заметки"
+                        title: "Мои заметки",
+                        // headerRight: () => (
+                        //     <Icon name="home" size={30} color="#900" />
+                        // )
                     }}
                 />
                 <Drawer.Screen
